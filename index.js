3edd392d1502cd637873623adfe453ae8ab35ba4
sushilsahoo06 <sahoosushil456@gmail.com>
2024-10-07 13:06:55 +0530

    hii

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing");
 const path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
 
 main().then((res)=>{
   console.log("connection succesfull !");
